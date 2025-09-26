82445c415b96aef818498641fbf17dbd767d4f9b
Phao98 <phao@berkeley.edu>
2025-09-25 23:45:39 -0700

    init setup and connect db
    第一次创建项目，完成了db的连接和基本的setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const connectDB = require('./db');
+
+const app = express();
+
+connectDB();
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
